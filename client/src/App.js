59ee0fb248ebd3df8e95923f6627b18cc067f84c
Muktafin-CSE22 <almukitmuktafin564@gmail.com>
2026-02-09 22:55:16 +0600

    Admin Dashboard Page

--- client/src/App.js
+++ client/src/App.js
@@ -13,6 +13,7 @@ function App() {
         <Route path="/auth/login" element={<Login />} /> */}
         
         {/* Other Routes */}
+        <Route path="/admin" element={<AdminPage />} />
       </Routes>
     </Router>
   );
